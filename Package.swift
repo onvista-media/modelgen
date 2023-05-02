67f6ce6743977126d4916584ab90322b656cc08a
Gereon Steffens <gereon.steffens@onvista.de>
2023-05-02 16:43:06 +0200

    add swiftlint

--- Package.swift
+++ Package.swift
@@ -7,25 +7,29 @@ let package = Package(
     name: "ModelGen",
     platforms: [.macOS(.v13)],
     products: [
-        .executable(name: "modelgen", targets: ["ModelGen"])
+        .executable(name: "modelgen", targets: ["modelgen"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "ModelGen",
+            name: "modelgen",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
-            path: "Sources"
+            path: "Sources",
+            plugins: [
+                .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
+            ]
         ),
         .testTarget(
             name: "modelgenTests",
-            dependencies: [ "ModelGen" ],
+            dependencies: [ "modelgen" ],
             path: "Tests"
         )
     ]
