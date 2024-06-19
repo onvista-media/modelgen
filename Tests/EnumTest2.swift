140428b5bc66ceb65bf7df1c535e30f5fa78e55c
Gereon Steffens <gereon.steffens@onvista.de>
2024-06-19 14:41:19 +0200

    fix test

--- Tests/EnumTest2.swift
+++ Tests/EnumTest2.swift
@@ -37,6 +37,56 @@ final class EnumTest2: XCTestCase {
     """
 
     private let expected = """
+    public struct Enum: Codable {
+        public let aEnum: AEnum?
+
+        public let bEnum: BEnum?
+
+        public init(aEnum: AEnum?, bEnum: BEnum?) {
+            self.aEnum = aEnum
+            self.bEnum = bEnum
+        }
+
+        enum CodingKeys: String, CodingKey {
+            case aEnum = "aEnum"
+            case bEnum = "bEnum"
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.aEnum = try container.decodeIfPresent(AEnum.self, forKey: .aEnum)
+            self.bEnum = try container.decodeIfPresent(BEnum.self, forKey: .bEnum)
+        }
+
+        public enum AEnum: String, Codable, CaseIterable, UnknownCaseRepresentable {
+            case plugh = "plugh"
+            case xyzzy = "xyzzy"
+
+            case _unknownCase
+            public static let unknownCase = Self._unknownCase
+
+            public static func make() -> Self {
+                .plugh
+            }
+        }
+
+        public enum BEnum: String, Codable, CaseIterable, UnknownCaseRepresentable {
+            case bar = "bar"
+            case baz = "baz"
+            case foo = "foo"
+
+            case _unknownCase
+            public static let unknownCase = Self._unknownCase
+
+            public static func make() -> Self {
+                .bar
+            }
+        }
+
+        public static func make(aEnum: AEnum? = nil, bEnum: BEnum? = nil) -> Self {
+            self.init(aEnum: aEnum, bEnum: bEnum)
+        }
+    }
     """
 
     func testEnumOrder() throws {
